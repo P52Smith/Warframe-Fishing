0ffaad215ee80b025aaf8d1d77cb2b0bbb64393d
Ben Smith <p52smith@hotmail.co.uk>
2017-11-11 12:50:54 +0000

    Implemented Fish classes

---
+++ Warframe Fishing/Fish.vb
@@ -0,0 +1,174 @@
+﻿Imports System.ComponentModel
+Imports Warframe_Fishing
+
+Public Class Fish
+    Implements IFish
+
+    Private Class _FishProducts
+        Implements IFishParts
+
+        Private _standing As FishSizes
+        Private _meat As FishSizes
+        Private _scales As FishSizes
+        Private _oil As FishSizes
+        Private _special As FishSizes
+        Private _specialName As String = "Unknown"
+
+        Public ReadOnly Property Standing As IFishSizes Implements IFishParts.Standing
+            Get
+                Return _standing
+            End Get
+        End Property
+
+        Public ReadOnly Property Meat As IFishSizes Implements IFishParts.Meat
+            Get
+                Return _meat
+            End Get
+        End Property
+
+        Public ReadOnly Property Scales As IFishSizes Implements IFishParts.Scales
+            Get
+                Return _scales
+            End Get
+        End Property
+
+        Public ReadOnly Property Oil As IFishSizes Implements IFishParts.Oil
+            Get
+                Return _oil
+            End Get
+        End Property
+
+        Public ReadOnly Property Special As IFishSizes Implements IFishParts.Special
+            Get
+                Return _special
+            End Get
+        End Property
+
+        Public ReadOnly Property SpecialName As String Implements IFishParts.SpecialName
+            Get
+                Return _specialName
+            End Get
+        End Property
+
+        Public Sub New(theStanding As FishSizes, theMeat As FishSizes, theScales As FishSizes, theOil As FishSizes, theSpecial As String)
+            _standing = theStanding
+            _meat = theMeat
+            _scales = theScales
+            _oil = theOil
+            _special = New FishSizes(1, 1, 1)
+            _specialName = theSpecial
+        End Sub
+    End Class
+    Private Class _FishParts
+        Implements IFishParts
+
+        Private ourFish As Fish
+
+        Public ReadOnly Property Standing As IFishSizes Implements IFishParts.Standing
+            Get
+                Return ourFish.Caught * ourFish.Products.Standing
+            End Get
+        End Property
+
+        Public ReadOnly Property Meat As IFishSizes Implements IFishParts.Meat
+            Get
+                Return ourFish.Caught * ourFish.Products.Meat
+            End Get
+        End Property
+
+        Public ReadOnly Property Scales As IFishSizes Implements IFishParts.Scales
+            Get
+                Return ourFish.Caught * ourFish.Products.Scales
+            End Get
+        End Property
+
+        Public ReadOnly Property Oil As IFishSizes Implements IFishParts.Oil
+            Get
+                Return ourFish.Caught * ourFish.Products.Oil
+            End Get
+        End Property
+
+        Public ReadOnly Property Special As IFishSizes Implements IFishParts.Special
+            Get
+                Return ourFish.Caught * ourFish.Products.Special
+            End Get
+        End Property
+
+        Public ReadOnly Property SpecialName As String Implements IFishParts.SpecialName
+            Get
+                Return ourFish.Products.SpecialName
+            End Get
+        End Property
+
+        Public Sub New(theFish)
+            ourFish = theFish
+        End Sub
+    End Class
+    Private _name As String = "Unknown"
+    Private _biome As Biomes
+    Private _activeTime As TimeOfDay
+    Private _spear As Spears
+    Private _bait As Baits = Baits.None
+    Private _products As _FishProducts
+    Private _caught As FishSizes
+    Private _productsAvailable As New _FishParts(Me)
+
+    Public ReadOnly Property Name As String Implements IFish.Name
+        Get
+            Return _name
+        End Get
+    End Property
+
+    Public ReadOnly Property Biome As Biomes Implements IFish.Biome
+        Get
+            Return _biome
+        End Get
+    End Property
+
+    Public ReadOnly Property ActiveTime As TimeOfDay Implements IFish.ActiveTime
+        Get
+            Return _activeTime
+        End Get
+    End Property
+
+    Public ReadOnly Property Spear As Spears Implements IFish.Spear
+        Get
+            Return _spear
+        End Get
+    End Property
+
+    Public ReadOnly Property Bait As Baits Implements IFish.Bait
+        Get
+            Return _bait
+        End Get
+    End Property
+
+    Public ReadOnly Property Products As IFishParts Implements IFish.Products
+        Get
+            Return _products
+        End Get
+    End Property
+
+    Public Property Caught As FishSizes Implements IFish.Caught
+        Get
+            Return _caught
+        End Get
+        Set(value As FishSizes)
+            _caught = value
+        End Set
+    End Property
+
+    Public ReadOnly Property ProductsAvailable As IFishParts Implements IFish.ProductsAvailable
+        Get
+            Return _productsAvailable
+        End Get
+    End Property
+
+    Public Sub New(theName As String, theBiome As Biomes, theActiveTime As TimeOfDay, theSpear As Spears, theBait As Baits, theStanding As FishSizes, theMeat As FishSizes, theScales As FishSizes, theOil As FishSizes, theSpecialPartName As String)
+        _name = theName
+        _biome = theBiome
+        _activeTime = theActiveTime
+        _spear = theSpear
+        _products = New _FishProducts(theStanding, theMeat, theScales, theOil, theSpecialPartName)
+    End Sub
+End Class
--- Warframe Fishing/IFish.vb
+++ Warframe Fishing/IFish.vb
@@ -1,4 +1,6 @@
-﻿Public Enum Spears
+﻿Imports System.ComponentModel
+
+Public Enum Spears
     Lanzo
     Tulok
     Peram
@@ -20,11 +22,74 @@ Public Enum Baits
     Glappid
 End Enum
 
+Public Enum TimeOfDay
+    Day
+    Night
+    Both
+End Enum
+
 Public Interface IFishSizes
-    Property Small As UInteger
-    Property Medium As UInteger
-    Property Large As UInteger
+    ReadOnly Property Small As UInteger
+    ReadOnly Property Medium As UInteger
+    ReadOnly Property Large As UInteger
 End Interface
+
+Public Class FishSizes
+    Implements IFishSizes, INotifyPropertyChanged
+
+    Private _small As UInteger = 0
+    Private _medium As UInteger = 0
+    Private _large As UInteger = 0
+
+    Public Property Small As UInteger Implements IFishSizes.Small
+        Get
+            Return _small
+        End Get
+        Set(value As UInteger)
+            If _small <> value Then
+                _small = value
+                RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(NameOf(Small)))
+            End If
+        End Set
+    End Property
+
+    Public Property Medium As UInteger Implements IFishSizes.Medium
+        Get
+            Return _medium
+        End Get
+        Set(value As UInteger)
+            If _medium <> value Then
+                _medium = value
+                RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(NameOf(Medium)))
+            End If
+        End Set
+    End Property
+
+    Public Property Large As UInteger Implements IFishSizes.Large
+        Get
+            Return _large
+        End Get
+        Set(value As UInteger)
+            If _large <> value Then
+                _large = value
+                RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(NameOf(Large)))
+            End If
+        End Set
+    End Property
+
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+    Public Sub New(small As UInteger, medium As UInteger, large As UInteger)
+        Me.Small = small
+        Me.Medium = medium
+        Me.Large = large
+    End Sub
+
+    Public Shared Operator *(ByVal caught As FishSizes, ByVal base As IFishSizes) As IFishSizes
+        Return New FishSizes(caught.Small * base.Small, caught.Medium * base.Medium, caught.Large * base.Large)
+    End Operator
+End Class
+
 
 Public Interface IFishParts
     ReadOnly Property Standing As IFishSizes
@@ -38,9 +103,10 @@ End Interface
 Public Interface IFish
     ReadOnly Property Name As String
     ReadOnly Property Biome As Biomes
+    ReadOnly Property ActiveTime As TimeOfDay
     ReadOnly Property Spear As Spears
     ReadOnly Property Bait As Baits
     ReadOnly Property Products As IFishParts
-    Property Caught As IFishSizes
+    Property Caught As FishSizes
     ReadOnly Property ProductsAvailable As IFishParts
 End Interface
