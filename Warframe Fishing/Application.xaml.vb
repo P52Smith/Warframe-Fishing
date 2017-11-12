e0ffe797363b3c960f854ec3fcfc328bbf4c0b54
Ben Smith <p52smith@hotmail.co.uk>
2017-11-12 19:46:22 +0000

    Prep and changes for release

--- Warframe Fishing/Application.xaml.vb
+++ Warframe Fishing/Application.xaml.vb
@@ -3,4 +3,33 @@
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
 
+    Sub unhandledExceptionHandler(sender As Object, args As UnhandledExceptionEventArgs)
+        Dim ex As Exception = DirectCast(args.ExceptionObject, Exception)
+        Dim Result As New String("")
+        Dim st As StackTrace = New StackTrace(ex, True)
+        For Each sf As StackFrame In st.GetFrames
+            If sf.GetFileLineNumber() > 0 Then
+                Result += $"{IO.Path.GetFileName(sf.GetFileName)} ({sf.GetFileLineNumber()}){vbNewLine}"
+            End If
+        Next
+        MessageBox.Show($"Exception: {ex.Message}
+Thrown in: {ex.TargetSite.DeclaringType}.{ex.TargetSite.Name}
+
+{Result}
+Please contact P52Smith with this information
+
+Unfortunately, we have to close now, sorry", "An error has occurred", MessageBoxButton.OK, MessageBoxImage.Error)
+    End Sub
+
+    Public Shared ReadOnly Property appVersion As String
+        Get
+            If Deployment.Application.ApplicationDeployment.IsNetworkDeployed Then
+                Dim ourVersion = Deployment.Application.ApplicationDeployment.CurrentDeployment.CurrentVersion
+                Return $"v{ourVersion.Major}.{ourVersion.Minor}.{ourVersion.Build}.{ourVersion.Revision}"
+            Else
+                Return "Development build"
+            End If
+        End Get
+    End Property
+
 End Class
--- Warframe Fishing/MainWindow.xaml.vb
+++ Warframe Fishing/MainWindow.xaml.vb
@@ -9,20 +9,22 @@ Class MainWindow
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
+        ' Update title
+        Title += $" - {Application.appVersion}"
         ' Add the fish!       Name        Biome        Active during   Best Spear    Bait                     Standing                   Meat                    Scales                  Oil        Special Part
-        allFish.Add(New Fish("Charc Eel", Biomes.Lake, TimeOfDay.Both, Spears.Tulok, Baits.None, New FishSizes(25, 35, 50), New FishSizes(3, 3, 5), New FishSizes(2, 3, 4), New FishSizes(1, 2, 3), "Charc Electroplax"))
-        allFish.Add(New Fish("Mawfish", Biomes.Lake, TimeOfDay.Day, Spears.Tulok, Baits.None, New FishSizes(25, 35, 50), New FishSizes(3, 4, 5), New FishSizes(3, 2, 3), New FishSizes(1, 3, 4), "Mawfish Bones"))
-        allFish.Add(New Fish("Yogwun", Biomes.Landlocked, TimeOfDay.Day, Spears.Tulok, Baits.None, New FishSizes(25, 35, 50), New FishSizes(4, 5, 6), New FishSizes(1, 2, 3), New FishSizes(1, 2, 3), "Yogwun Stomach"))
-        allFish.Add(New Fish("Khut-Khut", Biomes.Landlocked, TimeOfDay.Day, Spears.Peram, Baits.None, New FishSizes(25, 35, 50), New FishSizes(2, 3, 3), New FishSizes(1, 2, 3), New FishSizes(3, 4, 5), "Khut-Khut Venom Sac"))
-        allFish.Add(New Fish("Goopolla", Biomes.Coastal, TimeOfDay.Both, Spears.Tulok, Baits.None, New FishSizes(25, 35, 50), New FishSizes(3, 3, 4), New FishSizes(2, 4, 5), New FishSizes(1, 2, 3), "Goopolla Spleen"))
-        allFish.Add(New Fish("Mortus Lungfish", Biomes.Landlocked, TimeOfDay.Night, Spears.Peram, Baits.None, New FishSizes(100, 125, 200), New FishSizes(3, 4, 5), New FishSizes(1, 2, 3), New FishSizes(4, 6, 8), "Mortus Horn"))
-        allFish.Add(New Fish("Tralok", Biomes.Coastal, TimeOfDay.Day, Spears.Tulok, Baits.None, New FishSizes(100, 125, 200), New FishSizes(2, 3, 4), New FishSizes(5, 7, 9), New FishSizes(1, 2, 3), "Tralok Eyes"))
-        allFish.Add(New Fish("Cuthol", Biomes.Landlocked, TimeOfDay.Night, Spears.Lanzo, Baits.Cuthol, New FishSizes(500, 625, 100), New FishSizes(5, 8, 11), New FishSizes(3, 4, 5), New FishSizes(2, 3, 4), "Cuthol Tendrils"))
-        allFish.Add(New Fish("Murkray", Biomes.Coastal, TimeOfDay.Both, Spears.Lanzo, Baits.Murkray, New FishSizes(500, 625, 1000), New FishSizes(1, 3, 5), New FishSizes(2, 4, 6), New FishSizes(7, 8, 9), "Murkray Liver"))
-        allFish.Add(New Fish("Sharrac", Biomes.Coastal, TimeOfDay.Both, Spears.Lanzo, Baits.Twighlight, New FishSizes(100, 125, 200), New FishSizes(2, 3, 4), New FishSizes(4, 6, 8), New FishSizes(2, 3, 4), "Sharrac Teeth"))
-        allFish.Add(New Fish("Karkina", Biomes.Coastal, TimeOfDay.Both, Spears.Tulok, Baits.Twighlight, New FishSizes(100, 125, 200), New FishSizes(1, 2, 3), New FishSizes(3, 4, 5), New FishSizes(4, 6, 8), "Karkina Antenna"))
-        allFish.Add(New Fish("Glappid", Biomes.Coastal, TimeOfDay.Night, Spears.Peram, Baits.Glappid, New FishSizes(1200, 1500, 2000), New FishSizes(4, 6, 8), New FishSizes(5, 7, 9), New FishSizes(3, 5, 7), "Seram Beetle Shell"))
-        allFish.Add(New Fish("Norg", Biomes.Lake, TimeOfDay.Night, Spears.Peram, Baits.Norg, New FishSizes(500, 625, 100), New FishSizes(2, 3, 4), New FishSizes(5, 7, 10), New FishSizes(3, 5, 6), "Norg Brain"))
+        AllFish.Add(New Fish("Charc Eel", Biomes.Lake, TimeOfDay.Both, Spears.Tulok, Baits.None, New FishSizes(25, 35, 50), New FishSizes(3, 3, 5), New FishSizes(2, 3, 4), New FishSizes(1, 2, 3), "Charc Electroplax"))
+        AllFish.Add(New Fish("Mawfish", Biomes.Lake, TimeOfDay.Day, Spears.Tulok, Baits.None, New FishSizes(25, 35, 50), New FishSizes(3, 4, 5), New FishSizes(3, 2, 3), New FishSizes(1, 3, 4), "Mawfish Bones"))
+        AllFish.Add(New Fish("Yogwun", Biomes.Landlocked, TimeOfDay.Day, Spears.Tulok, Baits.None, New FishSizes(25, 35, 50), New FishSizes(4, 5, 6), New FishSizes(1, 2, 3), New FishSizes(1, 2, 3), "Yogwun Stomach"))
+        AllFish.Add(New Fish("Khut-Khut", Biomes.Landlocked, TimeOfDay.Day, Spears.Peram, Baits.None, New FishSizes(25, 35, 50), New FishSizes(2, 3, 3), New FishSizes(1, 2, 3), New FishSizes(3, 4, 5), "Khut-Khut Venom Sac"))
+        AllFish.Add(New Fish("Goopolla", Biomes.Coastal, TimeOfDay.Both, Spears.Tulok, Baits.None, New FishSizes(25, 35, 50), New FishSizes(3, 3, 4), New FishSizes(2, 4, 5), New FishSizes(1, 2, 3), "Goopolla Spleen"))
+        AllFish.Add(New Fish("Mortus Lungfish", Biomes.Landlocked, TimeOfDay.Night, Spears.Peram, Baits.None, New FishSizes(100, 125, 200), New FishSizes(3, 4, 5), New FishSizes(1, 2, 3), New FishSizes(4, 6, 8), "Mortus Horn"))
+        AllFish.Add(New Fish("Tralok", Biomes.Coastal, TimeOfDay.Day, Spears.Tulok, Baits.None, New FishSizes(100, 125, 200), New FishSizes(2, 3, 4), New FishSizes(5, 7, 9), New FishSizes(1, 2, 3), "Tralok Eyes"))
+        AllFish.Add(New Fish("Cuthol", Biomes.Landlocked, TimeOfDay.Night, Spears.Lanzo, Baits.Cuthol, New FishSizes(500, 625, 100), New FishSizes(5, 8, 11), New FishSizes(3, 4, 5), New FishSizes(2, 3, 4), "Cuthol Tendrils"))
+        AllFish.Add(New Fish("Murkray", Biomes.Coastal, TimeOfDay.Both, Spears.Lanzo, Baits.Murkray, New FishSizes(500, 625, 1000), New FishSizes(1, 3, 5), New FishSizes(2, 4, 6), New FishSizes(7, 8, 9), "Murkray Liver"))
+        AllFish.Add(New Fish("Sharrac", Biomes.Coastal, TimeOfDay.Both, Spears.Lanzo, Baits.Twighlight, New FishSizes(100, 125, 200), New FishSizes(2, 3, 4), New FishSizes(4, 6, 8), New FishSizes(2, 3, 4), "Sharrac Teeth"))
+        AllFish.Add(New Fish("Karkina", Biomes.Coastal, TimeOfDay.Both, Spears.Tulok, Baits.Twighlight, New FishSizes(100, 125, 200), New FishSizes(1, 2, 3), New FishSizes(3, 4, 5), New FishSizes(4, 6, 8), "Karkina Antenna"))
+        AllFish.Add(New Fish("Glappid", Biomes.Coastal, TimeOfDay.Night, Spears.Peram, Baits.Glappid, New FishSizes(1200, 1500, 2000), New FishSizes(4, 6, 8), New FishSizes(5, 7, 9), New FishSizes(3, 5, 7), "Seram Beetle Shell"))
+        AllFish.Add(New Fish("Norg", Biomes.Lake, TimeOfDay.Night, Spears.Peram, Baits.Norg, New FishSizes(500, 625, 100), New FishSizes(2, 3, 4), New FishSizes(5, 7, 10), New FishSizes(3, 5, 6), "Norg Brain"))
     End Sub
 
     Private Sub CaughtSmall_Subtract_Click(sender As Primitives.RepeatButton, e As RoutedEventArgs)
